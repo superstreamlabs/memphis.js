367d3dc8356e3ce603a49a78ec6238db374e240c
idanasulinStrech <idan@memphis.dev>
2022-10-23 16:16:03 +0300

    readme fixes

--- examples/consumer.controller.ts
+++ examples/consumer.controller.ts
@@ -1,16 +1,15 @@
-import { Controller, Post, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { consumeMessage } from 'memphis-dev/nest';
 import type { Message } from 'memphis-dev/types';
 
 @Controller('auth')
-export class AuthController {
-    @Get('signup')
+export class ExampleController {
     @consumeMessage({
         stationName: '<station-name>',
         consumerName: '<consumer-name>',
         consumerGroup: ''
     })
-    async signup(message: Message) {
+    async messageHandler(message: Message) {
         console.log(message.getData().toString());
         message.ack();
     }
