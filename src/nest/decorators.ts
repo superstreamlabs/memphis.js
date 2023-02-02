e2ec41c70ff97fb9cec731443074c2d8b7a4e9dc
Obumuneme Nwabude <obumnwabude@gmail.com>
2023-02-02 12:14:35 +0100

    chore: add context to consumeMessage decorator

--- src/nest/decorators.ts
+++ src/nest/decorators.ts
@@ -1,6 +1,9 @@
 import { EventPattern } from '@nestjs/microservices';
 import { MemphisConsumerOption } from './interfaces';
 
-export const consumeMessage = (consumer: MemphisConsumerOption) => {
-    return EventPattern(consumer);
+export const consumeMessage = (
+  consumer: MemphisConsumerOption,
+  context: object
+) => {
+  return EventPattern(consumer, context);
 };
