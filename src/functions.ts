db8634ffe31f12a18f5451caac819b469068e882
John Peters <john@memphis.dev>
2023-11-13 21:00:37 -0600

    "I had some weird indentation so I fixed it"

--- src/functions.ts
+++ src/functions.ts
@@ -7,29 +7,29 @@ const createFunction = (memphis_event, eventHandler) => {
 
     for (const message of memphis_event.messages) {
         try {
-        const payload = Buffer.from(message.payload, 'base64');
-        const { processedMessage, processedHeaders } = eventHandler(payload, message.headers);
+            const payload = Buffer.from(message.payload, 'base64');
+            const { processedMessage, processedHeaders } = eventHandler(payload, message.headers);
 
-        if (processedMessage instanceof Uint8Array && processedHeaders instanceof Object) {
-            processedEvents.messages.push({
-            headers: processedHeaders,
-            payload: Buffer.from(processedMessage).toString('base64')
-            });
-        } else if (processedMessage === null && processedHeaders === null) {
-            continue;
-        } else if (processedMessage === null || processedHeaders === null) {
-            const errMsg = `processedMessage is of type ${typeof processedMessage} and processedHeaders is ${typeof processedHeaders}. Either both of these should be null or neither`;
-            throw new Error(errMsg);
-        } else {
-            const errMsg = "The returned processedMessage or processedHeaders were not in the right format. processedMessage must be Uint8Array and processedHeaders, Object";
-            throw new Error(errMsg);
-        }
+            if (processedMessage instanceof Uint8Array && processedHeaders instanceof Object) {
+                processedEvents.messages.push({
+                headers: processedHeaders,
+                payload: Buffer.from(processedMessage).toString('base64')
+                });
+            } else if (processedMessage === null && processedHeaders === null) {
+                continue;
+            } else if (processedMessage === null || processedHeaders === null) {
+                const errMsg = `processedMessage is of type ${typeof processedMessage} and processedHeaders is ${typeof processedHeaders}. Either both of these should be null or neither`;
+                throw new Error(errMsg);
+            } else {
+                const errMsg = "The returned processedMessage or processedHeaders were not in the right format. processedMessage must be Uint8Array and processedHeaders, Object";
+                throw new Error(errMsg);
+            }
         } catch (e) {
-        processedEvents.failedMessages.push({
-            headers: message.headers,
-            payload: message.payload,
-            error: e.message
-        });
+            processedEvents.failedMessages.push({
+                headers: message.headers,
+                payload: message.payload,
+                error: e.message
+            });
         }
     }
 
