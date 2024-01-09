663c8c2ee4859a3aa1bfd510295a279166277b72
John Peters <john@memphis.dev>
2024-01-09 14:41:34 -0600

    "Consume now uses a for true in the example"

--- examples/consumer.js
+++ examples/consumer.js
@@ -8,7 +8,7 @@ const { memphis } = require('memphis-dev');
         let memphisConnection = await memphis.connect({
             host: "<memphis-host>",
             username: "memphis-username", // (root/application type user)
-            accountId: <memphis-accountId/>, //You can find it on the profile page in the Memphis UI. This field should be sent only on the cloud version of Memphis, otherwise it will be ignored
+            accountId: <memphis-accountId />, //You can find it on the profile page in the Memphis UI. This field should be sent only on the cloud version of Memphis, otherwise it will be ignored
             password: "<memphis-password>"
         });
 
@@ -17,17 +17,21 @@ const { memphis } = require('memphis-dev');
             consumerName: "<consumer-name>"
         })
 
-        let messages = consumer.fetch()
+        while (true) {
+            let messages = consumer.fetch({})
 
-        console.log(messages.length)
-        for (let message of messages){
-            const messageObject = JSON.parse(message.getData().toString());
-            // Do something with the message
-            console.log(messageObject["Hello"]);
-            message.ack()
+            if (messages.length == 0) {
+                continue;
+            }
+
+            for (let message of messages) {
+                const messageObject = JSON.parse(message.getData().toString());
+                // Do something with the message
+                console.table(messageObject);
+                message.ack();
+            }
+
         }
-
-        memphisConnection.close()
     } catch (ex) {
         console.log(ex);
         if (memphisConnection) memphisConnection.close();
